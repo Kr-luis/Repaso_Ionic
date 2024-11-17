4c5c3f7c30869bf7341d1372d3de6e46d02c02d6
Kr-luis <kluis816@gmail.com>
2024-11-16 23:43:00 -0500

    Login y chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+// Redirigir usuarios no autorizados al login
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/']);
+
+// Redirigir automáticamente a usuarios ya logueados
+const redirectLoggedInToChat = () => redirectLoggedInTo(['/chat']);
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToChat),
   },
   {
     path: 'chat',
-    loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
+    loadChildren: () => import('./pages/chat/chat.module').then(m => m.ChatPageModule),
+    ...canActivate(redirectUnauthorizedToLogin),
   },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full',
+  }
 ];
 
 @NgModule({
--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
+import { Observable, of } from 'rxjs'; // Usamos `of` para inicializar mensajes
+import { ChatService } from '../../services/chat.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  @ViewChild(IonContent) content: IonContent | undefined = undefined; // Inicialización a undefined
+  messages: Observable<any[]> = of([]); // Inicialización con un array vacío
+  newMsg = '';
 
-  constructor() { }
+  constructor(private chatService: ChatService, private router: Router) { }
 
   ngOnInit() {
+    this.messages = this.chatService.getChatMessages();
   }
 
+  sendMessage() {
+    if (this.newMsg.trim() !== '') {
+      this.chatService.addChatMessage(this.newMsg).then(() => {
+        this.newMsg = '';
+        if (this.content) {
+          this.content.scrollToBottom(300);
+        }
+      });
+    }
+  }
+
+  signOut() {
+    this.chatService.signOut().then(() => {
+      this.router.navigateByUrl('/', { replaceUrl: true });
+    });
+  }
 }
